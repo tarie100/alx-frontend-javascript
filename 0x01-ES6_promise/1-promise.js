f5e61e7e80999da817daaf2befab0d3477a35035
tarie100 <tarie.school@gmail.com>
2024-04-10 14:07:52 +0000

    1-promis

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,12 @@
-export default function getFullResponseFromAPI(success){
-	return new Promise((resolve, reject) => {
-		if (success){
-			resolve({status: 200, body: 'Success'});
-		}else{
-			reject(new Error('The Fake API is bot working currently'));
-		}
-	});
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
-
