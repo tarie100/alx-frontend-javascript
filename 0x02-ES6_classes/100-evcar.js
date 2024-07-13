aed9be846f16a74a5047b315eb9212fe4b89bdc9
tarie100 <125449345+tarie100@users.noreply.github.com>
2024-07-13 10:03:26 +0200

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,33 +1,13 @@
+/* eslint-disable */
 import Car from './10-car';
 
-/**
- * Represents a class EVCar.
- */
 export default class EVCar extends Car {
-  /**
-   * Creates a new Building instance.
-   *
-   * @param {String} brand - The brand of the car.
-   * @param {String} motor - The motor of the car.
-   * @param {String} color - The color of the car.
-   * @param {String} range - The range of the car.
-   */
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
-  }
-
-  get range() {
-    return this._range;
-  }
-
-  set range(value) {
-    this._range = value;
+    this._range = range;
   }
 
   cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+    return new Car();
   }
 }
