b3647d1c1af6d8d85b1aad6fd81013e8e0370ad0
tarie100 <125449345+tarie100@users.noreply.github.com>
2024-07-13 09:54:06 +0200

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,6 +1,17 @@
 import Car from './10-car';
 
+/**
+ * Represents a class EVCar.
+ */
 export default class EVCar extends Car {
+  /**
+   * Creates a new Building instance.
+   *
+   * @param {String} brand - The brand of the car.
+   * @param {String} motor - The motor of the car.
+   * @param {String} color - The color of the car.
+   * @param {String} range - The range of the car.
+   */
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this.range = range;
