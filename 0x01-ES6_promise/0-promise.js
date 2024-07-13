1d2ac84c710699c690ff98426c02cd006ce7e3d4
tarie100 <125449345+tarie100@users.noreply.github.com>
2024-07-13 09:16:51 +0200

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,9 @@
-export default function getResponseFromAPI(){
-	return new Promise(() => {});
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
