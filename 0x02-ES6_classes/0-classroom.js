7cc47bfb8eefb473ad2833bc3c6769eb366c2ee1
tarie100 <125449345+tarie100@users.noreply.github.com>
2024-07-13 09:51:32 +0200

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,14 @@
-export default class ClassRoom{
-	constructor(maxStudentsSize){
-		this._maxStudentsSize = maxStudentsSize;
-	}
+/**
+ * Represents a class ClassRoom.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new ClassRoom instance.
+   * @constructor
+   * @param {Number} maxStudentsSize - The max num of
+   * students in a class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
