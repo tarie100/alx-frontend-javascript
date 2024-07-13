4a762434598932fbc945227b168f9133883f474f
tarie100 <125449345+tarie100@users.noreply.github.com>
2024-07-13 09:52:49 +0200

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,30 +1,26 @@
-export default class Currency{
-	constructor(code, name){
-		if (typeof code !== 'string' || typeof name !== 'string'){
-			throw new Error('Invalid attribute');
-		}
-		this._code = code;
-		this._name = name;
-	}
-	get code(){
-		return this._code;
-	}
-	set code(value){
-		if (typeof code !== 'string'){
-			throw new Error('must be a string');
-		}
-		this._code = value;
-	}
-	get name(){
-		return this._name;
-	}
-	set name(value){
-		if (typeof name !== 'string'){
-			throw new Error('must be a string');
-		}
-		this._name = value;
-	}
-	displayFullCurrency(){
-		return `${this._name} (${this._code})`;
-	}
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return (`${this._name} (${this._code})`);
+  }
 }
