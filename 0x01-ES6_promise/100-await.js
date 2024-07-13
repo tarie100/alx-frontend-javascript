7cb4a7d7f1cec00b443eb963fcecfef57ffa188b
tarie100 <125449345+tarie100@users.noreply.github.com>
2024-07-13 09:47:53 +0200

    Update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,19 +1,17 @@
-#!/usr/bin/env node
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    const uPPromise = await uploadPhoto();
-    const uCPromise = await createUser();
-    return {
-      photo: uPPromise,
-      user: uCPromise,
-    };
-  } catch
-  (error) {
-    return {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    return Promise.resolve({
+      photo, user,
+    });
+  } catch (err) {
+    return Promise.resolve({
       photo: null,
       user: null,
-    };
+    });
   }
 }
